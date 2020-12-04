99da7e374e2244346563bf676c74115372c4e6d8
Scott Langridge <scott.m.langridge@gmail.com>
2020-12-04 14:47:37 +0000

    Solved day 3 part 1

--- Day-03/day03-1.py
+++ Day-03/day03-1.py
@@ -1,10 +1,6 @@
 def main(raw_input):
-    # Parse input
-
-    # Solve problem
-
-    # Return solution
-    return None
+    tree_map = parse_input(raw_input)
+    return count_trees(tree_map, 3)
 
 
 def get_input(filename):
@@ -14,7 +10,28 @@ def get_input(filename):
 
 
 def parse_input(raw_input):
-    return raw_input
+    tree_map = []
+    for row in raw_input.splitlines():
+        row_map = [x == '#' for x in row]
+        tree_map.append(row_map)
+
+    return tree_map
+
+
+def count_trees(tree_map, x_increment):
+    pattern_width = len(tree_map[0])
+    pos_x = 0
+    pos_y = 0
+
+    tree_count = 0
+    while pos_y < len(tree_map):
+        if tree_map[pos_y][pos_x]:
+            tree_count += 1
+
+        pos_x = (pos_x + x_increment) % pattern_width
+        pos_y += 1
+
+    return tree_count
 
 
 if __name__ == '__main__':
