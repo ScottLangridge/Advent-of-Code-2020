c6fa284245a6d7ee7d1d297d639b6ad5a44b3781
Scott Langridge <scott.m.langridge@gmail.com>
2020-12-04 14:51:38 +0000

    Solved day 3 part 2

--- Day-03/day03-2.py
+++ Day-03/day03-2.py
@@ -1,10 +1,12 @@
 def main(raw_input):
-    # Parse input
+    tree_map = parse_input(raw_input)
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 
-    # Solve problem
+    tree_count_product = 1
+    for i in slopes:
+        tree_count_product *= count_trees(tree_map, i[0], i[1])
 
-    # Return solution
-    return None
+    return tree_count_product
 
 
 def get_input(filename):
@@ -14,7 +16,28 @@ def get_input(filename):
 
 
 def parse_input(raw_input):
-    return raw_input
+    tree_map = []
+    for row in raw_input.splitlines():
+        row_map = [x == '#' for x in row]
+        tree_map.append(row_map)
+
+    return tree_map
+
+
+def count_trees(tree_map, x_increment, y_increment):
+    pattern_width = len(tree_map[0])
+    pos_x = 0
+    pos_y = 0
+
+    tree_count = 0
+    while pos_y < len(tree_map):
+        if tree_map[pos_y][pos_x]:
+            tree_count += 1
+
+        pos_x = (pos_x + x_increment) % pattern_width
+        pos_y += y_increment
+
+    return tree_count
 
 
 if __name__ == '__main__':
