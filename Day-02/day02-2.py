0088b8a675727bdfbbd9233613cf9306aba6ee4a
Scott Langridge <scott.m.langridge@gmail.com>
2020-12-04 13:55:28 +0000

    Solved day 2 part 2

--- Day-02/day02-2.py
+++ Day-02/day02-2.py
@@ -1,10 +1,12 @@
 def main(raw_input):
-    # Parse input
+    records = [parse_record(x) for x in raw_input.splitlines()]
 
-    # Solve problem
+    invalid_count = 0
+    for i in records:
+        if is_valid(i):
+            invalid_count += 1
 
-    # Return solution
-    return None
+    return invalid_count
 
 
 def get_input(filename):
@@ -13,8 +15,21 @@ def get_input(filename):
     return raw_input
 
 
-def parse_input(raw_input):
-    return raw_input
+def parse_record(record):
+    policy, password = record.split(': ')
+    indexes, char = policy.split(' ')
+    indexes = [int(x) for x in indexes.split('-')]
+
+    return {'indexes': indexes, 'char': char, 'pass': password}
+
+
+def is_valid(record):
+    char_count = 0
+    for i in record['indexes']:
+        if record['pass'][i - 1] == record['char']:
+            char_count += 1
+
+    return char_count == 1
 
 
 if __name__ == '__main__':
