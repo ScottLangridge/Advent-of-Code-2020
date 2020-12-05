fe1dfc1a48f4ef5ab4d562c54e21fc1fe9a728f1
Scott Langridge <scott.m.langridge@gmail.com>
2020-12-05 10:40:08 +0000

    Solved day 5 part 1

--- Day-05/day05-1.py
+++ Day-05/day05-1.py
@@ -1,10 +1,8 @@
 def main(raw_input):
-    # Parse input
+    seat_coords = parse_boarding_passes(raw_input)
+    seat_ids = [get_seat_id(i) for i in seat_coords]
 
-    # Solve problem
-
-    # Return solution
-    return None
+    return max(seat_ids)
 
 
 def get_input(filename):
@@ -13,8 +11,21 @@ def get_input(filename):
     return raw_input
 
 
-def parse_input(raw_input):
-    return raw_input
+def parse_boarding_passes(raw_input):
+    seat_strings = raw_input.splitlines()
+
+    seat_coords = []
+    for i in seat_strings:
+        row_bin = i[:7].replace('F', '0').replace('B', '1')
+        col_bin = i[-3:].replace('L', '0').replace('R', '1')
+        row_int = int(row_bin, 2)
+        col_int = int(col_bin, 2)
+        seat_coords.append((row_int, col_int))
+    return seat_coords
+
+
+def get_seat_id(seat_coordinate):
+    return seat_coordinate[0] * 8 + seat_coordinate[1]
 
 
 if __name__ == '__main__':
