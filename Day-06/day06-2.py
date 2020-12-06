9a53a3a3d06bfc54a50f38d543c4ee67315fe9b6
Scott Langridge <scott.m.langridge@gmail.com>
2020-12-06 09:57:35 +0000

    Solved day 6 part 2

--- Day-06/day06-2.py
+++ Day-06/day06-2.py
@@ -1,10 +1,9 @@
 def main(raw_input):
-    # Parse input
+    group_responses = parse_input(raw_input)
+    group_yes_counts = [len(i) for i in group_responses]
+    total_yes_count = sum(group_yes_counts)
 
-    # Solve problem
-
-    # Return solution
-    return None
+    return total_yes_count
 
 
 def get_input(filename):
@@ -14,7 +13,19 @@ def get_input(filename):
 
 
 def parse_input(raw_input):
-    return raw_input
+    groups = [i.split('\n') for i in raw_input.split('\n\n')]
+    return [get_group_intersection(i) for i in groups]
+
+
+def get_group_intersection(group):
+    answers_in_common = group.pop()
+    while len(group) > 0:
+        answers_in_common = get_list_intersection(answers_in_common, group.pop())
+    return answers_in_common
+
+
+def get_list_intersection(lst1, lst2):
+    return [i for i in lst1 if i in lst2]
 
 
 if __name__ == '__main__':
