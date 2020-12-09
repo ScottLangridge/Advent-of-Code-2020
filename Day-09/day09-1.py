795f457c5d92667c747375c2d246feabd1782eea
Scott Langridge <scott.m.langridge@gmail.com>
2020-12-09 12:56:53 +0000

    Solved day 9 part 1

--- Day-09/day09-1.py
+++ Day-09/day09-1.py
@@ -1,10 +1,15 @@
 def main(raw_input):
-    # Parse input
+    window_size = 25
 
-    # Solve problem
+    xmas_data = parse_input(raw_input)
 
-    # Return solution
-    return None
+    i = window_size
+    while i < len(xmas_data):
+        if not index_is_valid(xmas_data, i, window_size):
+            return xmas_data[i]
+        i += 1
+
+    return False
 
 
 def get_input(filename):
@@ -14,7 +19,19 @@ def get_input(filename):
 
 
 def parse_input(raw_input):
-    return raw_input
+    return [int(i) for i in raw_input.splitlines()]
+
+
+def index_is_valid(xmas_data, index, window_size):
+    window_vals = xmas_data[index - window_size:index]
+    for num1 in window_vals:
+        for num2 in window_vals:
+            if num1 + num2 == xmas_data[index] and num1 != num2:
+                return True
+
+    return False
+
+
 
 
 if __name__ == '__main__':
