837dca4876d22e0f0a407562f2c057a77375b7a4
Scott Langridge <scott.m.langridge@gmail.com>
2020-12-12 09:50:35 +0000

    Solved day 12 part 1

--- Day-12/day12-1.py
+++ Day-12/day12-1.py
@@ -1,10 +1,22 @@
 def main(raw_input):
-    # Parse input
 
-    # Solve problem
+    compass_headings = {'N': 0, 'E': 90, 'S': 180, 'W': 270}
 
-    # Return solution
-    return None
+    route = parse_input(raw_input)
+    pos_x, pos_y = 0, 0
+    heading = 90
+
+    for i in route:
+        if i[0] == 'L':
+            heading = (heading - i[1]) % 360
+        elif i[0] == 'R':
+            heading = (heading + i[1]) % 360
+        elif i[0] == 'F':
+            pos_x, pos_y = move(pos_x, pos_y, heading, i[1])
+        else:
+            pos_x, pos_y = move(pos_x, pos_y, compass_headings[i[0]], i[1])
+
+    return pos_x + pos_y
 
 
 def get_input(filename):
@@ -14,7 +26,18 @@ def get_input(filename):
 
 
 def parse_input(raw_input):
-    return raw_input
+    return [[i[0], int(i[1:])] for i in raw_input.splitlines()]
+
+
+def move(pos_x, pos_y, heading, distance):
+    if heading == 0:
+        return (pos_x, pos_y - distance)
+    elif heading == 90:
+        return (pos_x + distance, pos_y)
+    elif heading == 180:
+        return (pos_x, pos_y + distance)
+    elif heading == 270:
+        return (pos_x - distance, pos_y)
 
 
 if __name__ == '__main__':
