5a1b2256e47b05392ecf4603fcf95e3a7b653493
Scott Langridge <scott.m.langridge@gmail.com>
2020-12-12 14:16:43 +0000

    Refactored day 8

--- day08/day08_1.py
+++ day08/day08_1.py
@@ -1,4 +1,38 @@
-from GameConsole import GameConsole
+class GameConsole:
+    def __init__(self, program):
+        self.instructions = {
+            'nop': self.nop,
+            'acc': self.acc,
+            'jmp': self.jmp
+        }
+
+        self.pc = 0
+        self.acc = 0
+
+        self.prog = program
+
+    def exec(self):
+        visited = []
+
+        while self.pc < len(self.prog):
+            if self.pc in visited:
+                return False, self.acc
+            visited.append(self.pc)
+
+            operator, operand = self.prog[self.pc]
+            self.instructions[operator](operand)
+            self.pc += 1
+
+        return True, self.acc
+
+    def nop(self, operand):
+        pass
+
+    def acc(self, operand):
+        self.acc += operand
+
+    def jmp(self, operand):
+        self.pc += operand - 1
 
 
 def main(raw_input):
