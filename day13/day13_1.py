e47237b768d09663628309d2991bb78c80c660a4
Scott Langridge <scott.m.langridge@gmail.com>
2020-12-13 12:44:47 +0000

    Solved day 13 part 1

--- day13/day13_1.py
+++ day13/day13_1.py
@@ -1,10 +1,13 @@
 def main(raw_input):
-    # Parse input
+    earliest_depart, busses = parse_input(raw_input)
 
-    # Solve problem
+    wait_time = {}
+    for bus in busses:
+        wait_time[bus - (earliest_depart % bus)] = bus
 
-    # Return solution
-    return None
+    least_wait = min(wait_time.keys())
+
+    return least_wait * wait_time[least_wait]
 
 
 def get_input(filename):
@@ -14,7 +17,10 @@ def get_input(filename):
 
 
 def parse_input(raw_input):
-    return raw_input
+    earliest_depart, busses = raw_input.splitlines()
+    busses = busses.split(',')
+    busses = list(filter(lambda a: a != 'x', busses))
+    return int(earliest_depart), [int(bus) for bus in busses]
 
 
 if __name__ == '__main__':
