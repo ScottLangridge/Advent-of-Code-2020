3dda16f8a63c3a15c24174b7f28f43ed839f5be1
Scott Langridge <scott.m.langridge@gmail.com>
2020-12-14 13:10:20 +0000

    Solved day 14 part 1

--- day14/day14_1.py
+++ day14/day14_1.py
@@ -1,10 +1,14 @@
 def main(raw_input):
-    # Parse input
+    memory, instructions = parse_input(raw_input)
+    mask = ''
 
-    # Solve problem
+    for i in instructions:
+        if isinstance(i, str):
+            mask = i
+        else:
+            memory[i[0]] = apply_mask(mask, i[1])
 
-    # Return solution
-    return None
+    return sum(memory)
 
 
 def get_input(filename):
@@ -14,7 +18,33 @@ def get_input(filename):
 
 
 def parse_input(raw_input):
-    return raw_input
+    instructions = raw_input.splitlines()
+    greatest_address = 0
+    for i in range(len(instructions)):
+        if 'mem' in instructions[i]:
+            address, value = instructions[i].split(' = ')
+            address = address[4:-1]
+            instructions[i] = [int(address), int(value)]
+            if int(address) > greatest_address:
+                greatest_address = int(address)
+        else:
+            instructions[i] = instructions[i][-36:]
+
+    memory = [0] * (greatest_address + 1)
+    return memory, instructions
+
+
+def apply_mask(mask, dev_val):
+    bin_val = list(dec_to_binary(dev_val))
+
+    for i in range(len(mask)):
+        if mask[i] != 'X':
+            bin_val[i] = mask[i]
+    return int(''.join(bin_val), 2)
+
+
+def dec_to_binary(value):
+    return '{0:036b}'.format(value)
 
 
 if __name__ == '__main__':
